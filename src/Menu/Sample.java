418a083d7fb251680e9e641804e0225fc551014b
USER <USER@LAPTOP-SSOQKENV>
2022-06-20 00:03:45 +0900

    6/20

---
+++ src/Menu/Sample.java
@@ -0,0 +1,17 @@
+package Menu;
+
+import java.util.Scanner;
+
+public class Sample {
+	public static void main(String args[]) {
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Hello Ryo");
+		System.out.println("Whtat your name?");
+		String name = sc.next();
+		System.out.println("Nice to meet you " + name + " Im happy to see you!");
+
+	}
+
+}
